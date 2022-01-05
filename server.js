6340164753c898461b76af7836f4983a54d32b09
mgholden <mgholden27@gmail.com>
2022-01-04 20:53:59 -0500

    Basic Framework

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const express = require('express')
 const app = express()
 const expressEjsLayouts = require('express-ejs-layouts')
 const mongoose = require ('mongoose')
+const db = mongoose.connection
+
+mongoURI = 'mongodb://localhost:27017/crate'
 
 //CONFIG 
-const PORT = 9400
+const PORT = 8008
+
+//INDEX
+app.get('/records', (req, res) => {
+    res.render('index.ejs', {Record})
+})
+
 //LISTEN
 app.listen(PORT, () => console.log(`The record spins!  On port ${PORT}`))
 
