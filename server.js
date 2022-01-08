8fc3cd9dec2ba7f2a45ab24b190f336fcd511caf
mgholden <mgholden27@gmail.com>
2022-01-08 14:56:50 -0500

    Work in my new view and create route

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 //DEPENDENCIES
 const express = require('express')
 const app = express()
-const expressEjsLayouts = require('express-ejs-layouts')
+const expressEjsLayout = require('express-ejs-layouts')
 const mongoose = require ('mongoose')
 const db = mongoose.connection
 
+const routeHit = (req,res,next) => {
+    console.log("A new route was just hit");
+    next()
+}
+
+app.use(routeHit)
+app.use(express.urlencoded({extended:false}))
+app.use(expressEjsLayout)
+app.set('view engine', 'ejs')
+
 mongoURI = 'mongodb://localhost:27017/crate'
+
+//DATA
+const records = ['Tron: Legacy']
 
 //CONFIG 
 const PORT = 8008
 
 //ROUTES
 //INDEX
-app.get('/records', (req, res) => {
-    res.send('Spin the Turntable')
+app.get('/records/', (req, res) => {
+    res.send(records)
 })
 
 //NEW
 app.get('/records/new', (req,res) => {
-    res.render('new')
+    res.render('new.ejs')
 })
-
-
 
 //FIND
 // app.find({})
@@ -35,6 +46,13 @@ app.get('/records/new', (req,res) => {
 //SHOW
 app.get('/records/:indexOfRecordsArray', (req, res) => {
     res.render('show', {record: [req.params.indexOfRecordsArray]})
+})
+
+//CREATE
+app.post('/records', (req, res) => {
+    console.log('Create route accessed!')
+    console.log('Req.body is', req.body)
+    res.send(req.body)
 })
 
 //LISTEN
