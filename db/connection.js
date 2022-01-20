2ea33ad0ccedda01ebfe749dbd995d7be4a48a78
mgholden <mgholden27@gmail.com>
2022-01-19 23:08:48 -0500

    Fix Attempt number 2

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoURI = process.env.NODE_ENV === 'production'
-? process.env.DB_URL : 'mongodb://localhost:27017/records'
+? process.env.MONGODB_URI : 'mongodb://localhost:27017/records'
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
